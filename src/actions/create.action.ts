42ea92c45f3b6702d123139579bf1547bbaea008
vigor13 <vigor13.dev@gmail.com>
2023-10-27 18:23:46 +0900

    feat: add default action to commander

---
+++ src/actions/create.action.ts
@@ -0,0 +1,5 @@
+import * as Commander from 'commander';
+
+export const createAction = async (directory: string | undefined) => {
+  console.log(directory);
+};
--- src/commands/command.loader.ts
+++ src/commands/command.loader.ts
@@ -1,3 +1,42 @@
 import * as Commander from 'commander';
+import * as packageJson from '../../package.json';
+import * as actioins from '../actions';
 
-export class CommandLoader {}
+export class CommandLoader {
+  private _program: Commander.Command;
+  private _pkgJson: any;
+  private _actions: any;
+
+  constructor() {
+    this._program = new Commander.Command();
+    this._pkgJson = packageJson;
+    this._actions = actioins;
+  }
+
+  private _buildDefaultAction = () => {
+    const { name, version, description } = this._pkgJson;
+    this._program.name(name).description(description).version(version);
+
+    this._program
+      .argument('[project-directory]', 'set project directory')
+      .action(this._actions.createAction);
+  };
+
+  private _build = () => {
+    this._buildDefaultAction();
+  };
+
+  private _parse = () => {
+    this._program
+      .parseAsync()
+      .then((res) => {})
+      .catch((reason) => {
+        console.log(reason);
+      });
+  };
+
+  public init = () => {
+    this._build();
+    this._parse();
+  };
+}
