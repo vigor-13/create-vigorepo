56a4ad9f5af5dde8749c0068dfbccc6d41921196
vigor13 <vigor13.dev@gmail.com>
2023-11-10 17:15:33 +0900

    feat: add default polyrepo templates

---
+++ templates/polyrepo/.eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: ['standard-with-typescript', 'prettier'],
+  tsconfigRootDir: __dirname,
+  rules: {},
+};
---
+++ templates/polyrepo/src/index.ts
@@ -0,0 +1,1 @@
+console.log('This is Default Polyrepo Template.');
